c7470392d28f166cd36a215fb65285dd2b3ad0c2
dawid <budzynski.dawid@gmail.com>
2018-06-30 22:15:01 +0200

    random colors - added

--- colorGame.js
+++ colorGame.js
@@ -1,29 +1,23 @@
-var colors = [
-    'rgb(255, 0, 0)',
-    'rgb(255, 255, 0)',
-    'rgb(0, 255, 0)',
-    'rgb(0, 255, 255)',
-    'rgb(0, 0, 255)',
-    'rgb(255, 0, 255)'
-]
-
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll('.square');
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.getElementById('message');
+var h1 = document.querySelector('h1');
 
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
-    // add initial colors to squres
-    squares[i].style.backgroundColor = colors[i]
+    // add initial colors to squares
+    squares[i].style.backgroundColor = colors[i];
 
     // add click listeners to squares
     squares[i].addEventListener('click', function () {
         var clickedColor = this.style.backgroundColor;
         if (clickedColor === pickedColor) {
-            messageDisplay.textContent = 'Correct !'
+            messageDisplay.textContent = 'Correct !';
             changeColors(clickedColor);
+            h1.style.background = clickedColor;
         } else {
             this.style.backgroundColor = '#232323';
             messageDisplay.textContent = 'Wrong Answer !'
@@ -38,6 +32,22 @@ function changeColors(color) {
 }
 
 function pickColor() {
-    var randomIndex = Math.floor(Math.random() * colors.length)
+    var randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex]
+}
+
+
+function generateRandomColors(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push(randomColor())
+    }
+    return arr;
+}
+
+function randomColor() {
+    var red = Math.floor(Math.random() * 256)
+    var green = Math.floor(Math.random() * 256)
+    var blue = Math.floor(Math.random() * 256)
+    return "rgb(" + red + ", " + green + ", " + blue + ")"
 }
